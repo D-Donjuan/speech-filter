508185c04111004ff5aa013f0a2fa404d6d773be
D-Donjuan <ddonjuan57@gmail.com>
2022-03-29 21:33:54 -0700

    completed re-arranging menu options structure

--- main.py
+++ main.py
@@ -1,52 +1,27 @@
-from menu import optionChoice
+from menu import optionChoice, showTitle, getTextFileName, selectStopWordFileOption
+from speechFilter import FileFilter
 
 
 def main():
-    # debateFile = open("algo.txt", "r")
-    # debateString = debateFile.read()
-    # debateFile.close()
+
+    showTitle()
+    print("First we will need you to provide the text file you would like to filter")
+    print("as well as the stopWords file if you choose to provide your own")
+    fileToFilter = getTextFileName()
+    stopWordsFile = selectStopWordFileOption()
+    
+    debateFile = open(fileToFilter, "r")
+    fileFilterString = debateFile.read()
+    debateFile.close()
 
 
-    # stopWordsFile = open("stopWords.txt", "r")
-    # stopWordsString = stopWordsFile.read()
-    # stopWordsFile.close()
+    stopWordsFile = open(stopWordsFile, "r")
+    stopWordsString = stopWordsFile.read()
+    stopWordsFile.close()
 
-    # firstInst = FileFilter(debateString, stopWordsString)
-    # firstInst.removePunctuations()
-    # firstInst.removeStopWords()
-    # firstInst.countUniqueWords()
-    # firstInst.historyOfFilteredFiles()
-    # firstInst.testReadStopWordsFile()
+    userInstance = FileFilter(fileFilterString, stopWordsString)
+    optionChoice(userInstance)
 
-    # finishInput = False
-    # deleteWordsList = []
-
-    # while not finishInput:
-    #     deleteWord = input("Please enter the word you want to delete. Type :q to finish: ")
-    #     if deleteWord == ":q":
-    #         firstInst.removeStopWordsFromFile(deleteWordsList)
-    #         finishInput = True
-    #     else:
-    #         deleteWordsList.append(deleteWord)
-
-    # deleteWordsList = []
-
-    # addWordsDone = False
-    # addWordsList = []
-
-    # while not addWordsDone:
-    #     addWord = input("Please enter the word you want to add. Type :q to finish: ")
-    #     if addWord == ":q":
-    #         firstInst.addWordToFile(addWordsList)
-    #         addWordsDone = True
-    #     else:
-    #         addWordsList.append(addWord)
-        
-    # addWordsList = []
-        
-    # firstInst.testReadStopWordsFile()
-
-    optionChoice()
 
 
 if __name__ == "__main__":
